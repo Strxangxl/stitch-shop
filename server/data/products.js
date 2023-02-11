8f4eaeb03f95133d33f00af89ebd8aa570a19267
Ratandeep <chaddharatandeepsingh2345@gmail.com>
2023-02-11 21:51:23 +0530

    images stuff

--- server/data/products.js
+++ server/data/products.js
@@ -1,91 +1,91 @@
 const products = [
   {
     name: "Black Leather Jacket",
-    category: 'Jackets',
+    category: "Jackets",
     price: 89.99,
     countInStock: 10,
     rating: 4.7,
     numReviews: 13,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image:
-      "./assets/1a.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "/assets/1a.png",
   },
   {
     name: "Half-Black Leather Jacket",
-    category: 'Jackets',
-    price: 20.50,
+    category: "Jackets",
+    price: 20.5,
     countInStock: 7,
     rating: 3.2,
     numReviews: 4,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image:
-      "./assets/2a.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "/assets/2a.png",
   },
   {
     name: "Full Shiny Trunk",
     category: "Lowers",
-    price: 32.50,
+    price: 32.5,
     countInStock: 5,
     rating: 3,
     numReviews: 2,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image:
-    "./assets/3a.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "/assets/3a.png",
   },
   {
     name: "Beautiful Yellow Dress",
-    category: 'Outfit',
-    price: 90.00,
+    category: "Outfit",
+    price: 90.0,
     countInStock: 6,
     rating: 43.8,
     numReviews: 9,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image:
-    "./assets/4a.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "/assets/4a.png",
   },
   {
     name: "Black Frock",
     category: "Outfit",
-    price: 95.00,
+    price: 95.0,
     countInStock: 20,
     rating: 4.5,
     numReviews: 13,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image:
-    "./assets/5a.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "/assets/5a.png",
   },
   {
     name: "Summer Half Wear",
     category: "Fashion",
-    price: 40.00,
+    price: 40.0,
     countInStock: 11,
     rating: 4.4,
     numReviews: 23,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image:
-    "./assets/6a.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "/assets/6a.png",
   },
   {
     name: "Pista Green Dress",
     category: "Fashion",
-    price: 100.00,
+    price: 100.0,
     countInStock: 20,
     rating: 4.8,
     numReviews: 13,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image:
-    "./assets/7a.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "/assets/7a.png",
   },
   {
     name: "Summer Outfits",
     category: "Outfit",
-    price: 30.00,
-    countInStock:8,
+    price: 30.0,
+    countInStock: 8,
     rating: 4.1,
     numReviews: 6,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image:
-    "./assets/8a.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "/assets/8a.png",
   },
   {
     name: "Half body black tshirt",
@@ -94,21 +94,21 @@ const products = [
     countInStock: 3,
     rating: 4.4,
     numReviews: 3,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image:
-      "./assets/9a.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "/assets/9a.png",
   },
   {
     name: "Half body white tshirt",
     category: "Upper",
-    price: 60.00,
+    price: 60.0,
     countInStock: 1,
     rating: 4.2,
     numReviews: 5,
-    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-    image:
-    "./assets/10a.png",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: "./assets/10a.png",
   },
 ];
 
-export default products
+export default products;
